84352fbb9e919a4962adc1bd098722fc504754e1
Mudit-The-Great <Mudit-The-Great@users.noreply.github.com>
2024-08-18 19:10:13 +0530

    created initial project

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for the DOM to be fully loaded before running the script
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript is linked and running!');
+    // You can add more JavaScript code here
+});
